14b4600f534facce2c84df8acbbe96cd7679d082
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2023-06-02 16:41:09 -0400

    Premiere version qui compile et démarre en utilisant les stubs d'OpenApi.

---
+++ src/main/java/ca/ulaval/set/apipoc/SpringApipocApplication.java
@@ -0,0 +1,13 @@
+package ca.ulaval.set.apipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApipocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApipocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/ulaval/set/apipoc/springserver/SpringApipocApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.ulaval.set.apipoc.springserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApipocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
