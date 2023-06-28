bc20ec37403852a33c308156ba4342a5ed34e1eb
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2023-06-28 16:22:36 -0400

    Supprime les annotations en trop.

--- ul-apipoc-admission/ul-apipoc-admission-domaine/src/main/java/ca/ulaval/set/apipoc/admission/application/in/dossierAdmission/RechercherEtablissementEnseignementFrequenteQueryPort.java
+++ ul-apipoc-admission/ul-apipoc-admission-domaine/src/main/java/ca/ulaval/set/apipoc/admission/application/in/dossierAdmission/RechercherEtablissementEnseignementFrequenteQueryPort.java
@@ -1,15 +1,13 @@
 package ca.ulaval.set.apipoc.admission.application.in.dossierAdmission;
 
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 public interface RechercherEtablissementEnseignementFrequenteQueryPort {
-    @Transactional
-    @NotNull
-    @Valid List<EtablissementEnseignementFrequenteEntiteDto> apply(
+
+    @NotNull List<EtablissementEnseignementFrequenteEntiteDto> apply(
             @NotNull @Valid RechercheEtablissementEnseignementFrequenteCmdDto rechercheCmdDto);
 
     record RechercheEtablissementEnseignementFrequenteCmdDto(
